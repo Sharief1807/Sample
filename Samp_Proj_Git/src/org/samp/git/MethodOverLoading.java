701d6edb4d6b9d2435d72661a2b0670ac3cc4983
Sharief1807 <sharief.indian@gmail.com>
2019-04-03 00:42:06 +0530

    changes commited

--- Samp_Proj_Git/src/org/samp/git/MethodOverLoading.java
+++ Samp_Proj_Git/src/org/samp/git/MethodOverLoading.java
@@ -1,29 +1,27 @@
 package org.samp.git;
 
 public class MethodOverLoading {
-	
-	public void sha() {
-System.out.println("zero parameterized method");
 
-}
-	
+	public void sha() {
+		System.out.println("zero parameterized method");
+
+	}
+
 	public void sha(String str) {
 		System.out.println("string parameterized method");
 
-		}
-	
+	}
+
 	public void sha(int i) {
 		System.out.println("integer parameterized method");
 
-		}
-	
+	}
+
 	public static void main(String[] args) {
-		MethodOverLoading m1=new MethodOverLoading();
+		MethodOverLoading m1 = new MethodOverLoading();
 		m1.sha();
 		m1.sha(56);
 		m1.sha("sachin");
 	}
-	
-	
 
 }
